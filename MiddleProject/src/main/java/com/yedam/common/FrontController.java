14eecd9f2f68d627478a571c5288422e2c5c75d2
user <user@DESKTOP-9HPM1AG>
2023-05-02 16:30:08 +0900

    05020430

---
+++ MiddleProject/src/main/java/com/yedam/common/FrontController.java
@@ -0,0 +1,54 @@
+package com.yedam.common;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FrontController extends HttpServlet{
+	private Map<String, Control> map;
+	String encoding;
+	public FrontController() {
+		map = new HashMap<>();
+	}
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		encoding = config.getInitParameter("enc");
+		// 첫페이지
+		map.put("/main.do", new MainControl());
+	}
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.setCharacterEncoding(encoding);
+		
+		String uri = req.getRequestURI();
+		String context = req.getContextPath();
+		String path = uri.substring(context.length());
+		System.out.println(path);
+		
+		Control control = map.get(path);
+		String viewPage = control.execute(req, resp);
+		System.out.println(viewPage);
+		
+		if(viewPage.endsWith(".do")) {
+			resp.sendRedirect(viewPage);
+			return;		//메소드종료
+		} 
+		if(viewPage.endsWith(".json")) {
+			resp.setContentType("text/json;charset=UTF-8");
+			resp.getWriter().print(viewPage.substring(0, viewPage.length()-5));
+			return;
+		}
+		
+		//페이지 재지정
+		RequestDispatcher rd = req.getRequestDispatcher(viewPage);
+		rd.forward(req, resp);
+		
+	}
+}
