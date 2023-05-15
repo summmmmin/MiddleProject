1400eeb44b742e0d75a8cd5b94ffec5da2b3de15
user <user@DESKTOP-9HPM1AG>
2023-05-15 12:53:39 +0900

    판매상태변경 수정, 구매 판매 로그인 추가

--- MiddleProject/src/main/java/com/yedam/buy/control/BuyPriceControl.java
+++ MiddleProject/src/main/java/com/yedam/buy/control/BuyPriceControl.java
@@ -5,16 +5,24 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.yedam.common.Control;
 import com.yedam.sell.domain.SellVO;
 import com.yedam.sell.service.SellService;
 import com.yedam.sell.service.SellServiceImpl;
+import com.yedam.user.domain.UserVO;
 
 public class BuyPriceControl implements Control {
 
 	@Override
 	public String execute(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		HttpSession session = req.getSession();
+	    UserVO user = (UserVO) session.getAttribute("userinfo"); //로그인된 정보
+	    if(user == null) {
+	    	return "loginForm.do";
+	    }
+		
 		String prodId = req.getParameter("prodId");
 		String size = req.getParameter("size");
 		
--- MiddleProject/src/main/java/com/yedam/sell/control/SellUpdateControl.java
+++ MiddleProject/src/main/java/com/yedam/sell/control/SellUpdateControl.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.yedam.buy.domain.BuyVO;
+import com.yedam.buy.service.BuyService;
+import com.yedam.buy.service.BuyServiceImpl;
 import com.yedam.common.Control;
 import com.yedam.sell.domain.SellVO;
 import com.yedam.sell.service.SellService;
@@ -24,23 +27,39 @@ public class SellUpdateControl implements Control {
 		String dlvy = req.getParameter("dlvy");
 		String cancel = req.getParameter("cancel");
 		
-		vo.setSellId(Integer.parseInt(sellId));
+		SellService service = new SellServiceImpl();
+		vo = service.getSellId(Integer.parseInt(sellId));
+		
 		vo.setSellDlvy(dlvy);
 		vo.setSellCancel(cancel);
-		
-		SellService service = new SellServiceImpl();
+
 		boolean result = false;
-		
+		BuyService buyservice = new BuyServiceImpl();
 		String json = "";
-		
+		BuyVO buy = new BuyVO();
+		buy.setBuyId(vo.getBuyId());
+		buy.setBuyCancel(cancel);
+		buy.setBuyDlvy(dlvy);
+		System.out.println(buy);
+		System.out.println(dlvy);
+		System.out.println("검수합격".equals(dlvy));
+		System.out.println(dlvy.equals("검수합격"));
 		Map<String, Object> map = new HashMap<>();
 		
 		if(cancel != null) {
+			if(cancel.equals("취소완료")) {
+				boolean test= buyservice.cancelBuy(buy);
+				System.out.println(test);
+			}
 			result= service.cancelSell(vo);
 			
 		}
 		
 		if( dlvy != null) {
+			if(dlvy.equals("입고완료")|| dlvy.equals("검수중") || dlvy.equals("검수합격")|| dlvy.equals("검수불합격")|| dlvy.equals("거래실패")) {
+				boolean test1=buyservice.deliveBuy(buy);
+				System.out.println(test1);
+			}
 			result = service.dlvySell(vo);
 			
 		}
